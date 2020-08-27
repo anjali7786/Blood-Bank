2b1802b23d0806d6c9a514591212361ae36baeca
ankh77sb <ankh77sb@gmail.com>
2020-08-27 14:04:15 +0530

    register authentictaion,login authentication and reset password done!

---
+++ Locating_blood_centre/app/src/main/java/com/example/locating_blood_centre/mainpersonalacc.java
@@ -0,0 +1,14 @@
+package com.example.locating_blood_centre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainpersonalacc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpersonalacc);
+    }
+}
